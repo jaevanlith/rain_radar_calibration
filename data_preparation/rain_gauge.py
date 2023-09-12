60bbb09cd4749fe64b20c644a0d77c329e2cb1e3
ZENBOOK-VAN-JOC\Jochem van Lith <j.a.e.vanlith@student.tudelft.nl>
2023-09-12 12:30:34 +0700

    Integrate hourly conversion

--- data_preparation/rain_gauge.py
+++ data_preparation/rain_gauge.py
@@ -1,13 +1,74 @@
 import pandas as pd
 
-def load_rain_gauge_data():
+# ONLY FOR TESTING (set to None while running)
+NROWS = 10000
+
+def load_rain_gauge_data(rain_gauge_data_path, year):
     '''
-    Method to load data from excel files.
+    Method to load data from excel files per year.
+
+    @param rain_gauge_data_path str: Directory containing the rain gauge data.
+    @param year int: Year to analyse the data from.
+
+    @return rain_HII DataFrame: HII rain data sampled every 10 mins.
+    @return location_HII DataFrame: Locations of HII stations.
+    @return rain_EWS DataFrame: EWS rain data sampled every 15 mins.
+    @return location_EWS DataFrame: Locations of EWS stations.
     '''
 
-    rain_HII_2022 = pd.read_excel('data/raingauge_HII_Phetchaburi_10min_2022.xlsx', index_col=0, parse_dates=True, nrows=10)
-    location_HII = pd.read_excel('data/HII_location.xlsx', index_col=0, nrows=10)
-    
-    return rain_HII_2022, location_HII
+    # Load HII data
+    rain_HII = pd.read_excel(rain_gauge_data_path + '/HII_10min/raingauge_HII_Phetchaburi_10min_' + str(year) + '.xlsx', index_col=0, parse_dates=True, nrows=NROWS)
+    location_HII = pd.read_excel(rain_gauge_data_path + '/HII_location.xlsx', index_col=0, nrows=NROWS)
 
-#TODO add Kagan analysis to filter out unreliable data
+    # Load EWS data
+    rain_EWS = pd.read_csv(rain_gauge_data_path + '/EWS_15min/EWS_station_phetchaburi_project_15m_' + str(year) + '.csv', index_col=0, parse_dates=['Datetime'], date_format='%d/%m/%Y %H:%M', nrows=NROWS)
+    location_EWS = pd.read_excel(rain_gauge_data_path + '/EWS_location.xlsx', index_col=0, nrows=NROWS)
+
+    return rain_HII, location_HII, rain_EWS, location_EWS
+
+
+def convert_and_merge(rain_HII_10min, rain_EWS_15min):
+    '''
+    Method to convert HII and EWS dataframes to hours and merge them.
+
+    @param rain_HII_10min DataFrame: HII rain data sampled every 10 mins.
+    @param rain_EWS_15min DataFrame: EWS rain data sampled every 15 mins.
+
+    @return rain_merged_60min DataFrame: Combined data sampled every 60 mins.
+    '''
+
+    # Convert HII to hours
+    rain_HII_60min = rain_HII_10min.resample('H').agg(pd.Series.sum, skipna=False)
+    rain_HII_60min.reset_index(inplace=True)
+
+    # Convert EWS to hours
+    rain_EWS_60min = rain_EWS_15min.resample('H').agg(pd.Series.sum, skipna=False)
+    rain_EWS_60min.reset_index(inplace=True)
+
+    # Merge
+    rain_merged_60min = pd.merge(rain_HII_60min, rain_EWS_60min, on='Datetime', how='outer')
+    rain_merged_60min.set_index('Datetime', inplace=True)
+
+    return rain_merged_60min
+
+
+def prepare_rain_gauge_data(rain_gauge_data_path, year):
+    '''
+    Method to prepare rain gauge data entirely.
+
+    @param rain_gauge_data_path str: Directory containing the rain gauge data.
+    @param year int: Year to analyse the data from.
+
+    @return rain_gauge_data DataFrame: Final rain gauge data passed to next component.
+    '''
+
+    # Load HII and EWS data from files
+    rain_HII_10min, location_HII, rain_EWS_15min, location_EWS = load_rain_gauge_data(rain_gauge_data_path, year)
+
+    # Convert data to hours and merge HII and EWS
+    rain_merged_60min = convert_and_merge(rain_HII_10min, rain_EWS_15min)
+
+    #TODO add Kagan analysis to filter out unreliable data (replace placeholder)
+    rain_gauge_data = rain_merged_60min
+
+    return rain_gauge_data
--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import argparse
-from data_preparation.rain_gauge import load_rain_gauge_data
+from data_preparation.rain_gauge import prepare_rain_gauge_data
 from event_selection import select_all_events
 from calibration import calibrate
 
 if __name__ == '__main__':
-    # Parse ommand line arguments
+    # Parse command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--example', type=str, default="hello")
+    parser.add_argument('--rain_gauge_data_path', type=str, default="./data/rain_gauge")
+    parser.add_argument('--radar_data_path', type=str, default="./data/radar")
+    parser.add_argument('--year', type=int, default=2022)
+    parser.add_argument('--k', type=int, default=2, help='Rainfall threshold for event selection')
+    parser.add_argument('--l', type=int, default=1, help='Minimum number of hours that an event should have')
+    parser.add_argument('--h', type=int, default=2, help='Maximum number of hours no rain within one event')
     args = dict(vars(parser.parse_args()))
+    
+    ########## DATA PREPARATION ###########
+    # Initialize provided arguments
+    rain_gauge_data_path = args['rain_gauge_data_path']
+    radar_data_path = args['radar_data_path']
+    year = args['year']
 
-    # Load data from data preparation
-    rain_HII_2022, location_HII = load_rain_gauge_data()
+    # Prepare rain gauge data in hours
+    rain_gauge_data = prepare_rain_gauge_data(rain_gauge_data_path, year)
 
-    # Select events
-    events = select_all_events(rain_HII_2022)
+    ############ EVENT SELECTION ##########
+    #Initialize provided arguments
+    k = args['k']
+    l = args['l']
+    h = args['h']
 
-    # Calibration
+    # Select events based on prepared data
+    events = select_all_events(rain_gauge_data, k, l, h)
+
+    ########### CALIBRATION ###########
     A, b = calibrate(events)
