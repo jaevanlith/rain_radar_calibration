2fa1bc975fd6c36799a5c305304f666feb769570
ZENBOOK-VAN-JOC\Jochem van Lith <j.a.e.vanlith@student.tudelft.nl>
2023-10-12 15:26:21 +0700

    Update radar read png

--- data_preparation/radar.py
+++ data_preparation/radar.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from PIL import Image
 import rasterio
 import os
 import warnings
@@ -58,68 +59,77 @@ def prepare_radar_data(radar_data_path, year, noise_threshold, hail_threshold):
     subset = station_loc[['Name', 'pixel_y','pixel_x']]
     location_list = [tuple(x) for x in subset.to_numpy()]
 
-    # Get list of radar files and count
-    filelist = os.listdir(radar_data_path + '/radar_csv/' + str(year))
-    num_files = len(filelist)
+    # Set the root path of the year under investigation
+    radar_png_path = radar_data_path + '/radar_png/' + str(year)
 
-    # Create dataframe
-    indices = []
-    for i in range(1, num_files+1):
-        indices.append(i)
-    
-    columns = []
-    for j in location_list:
-        columns.append(j[0])
-    df = pd.DataFrame(index=indices,columns=columns)
-
+    # Init dataframe to store radar data
+    columns = [x[0] for x in location_list]
+    radar_df = pd.DataFrame(columns=columns)
     # Init datetime column
     DateTime = []
-    # Loop over all radar files
-    for file in filelist:
-        # Get datetime from file name   
-        Year = '20' + file[14:16]
-        Month = file[16:18]
-        Day = file[18:20]
-        Hour = file[20:22]
-        Minute = file[22:24]
-        # Format datetime
-        Date = Year + "-" + Month + "-" + Day + ' ' + Hour + ':' + Minute
-        DateTime.append(Date)
 
-        # Load radar data from file
-        extract_data = np.arange(len(location_list), dtype=float)
-        df_data = pd.read_csv(radar_data_path + '/radar_csv/' + str(year) + '/' + file, skiprows=3, index_col=None, header=None)
+    # Loop over months
+    for month in range(1,2):
+        # Set path for this month
+        radar_png_month_path = radar_png_path + '/' + f"{month:02}"
+        # Loop over days
+        # for day in [x[0] for x in os.walk(radar_png_month_path)]:
+        for day in [1]:
+            # Set path for this day
+            radar_png_day_path = radar_png_month_path + '/' + f"{day:02}"
+            # Get list of files and count
+            filelist = os.listdir(radar_png_day_path)
 
-        # Loop over locations of stations
-        for location in location_list:
-            # Get the data corresponding to the pixel locations
-            pixel_x = int(location[2])
-            pixel_y = int(location[1])
-            value = df_data.iloc[pixel_y, pixel_x]
-            extract_data[location_list.index(location)] = value
+            # Loop over all radar files
+            for file in filelist:
+                # Get datetime from file name   
+                Year = file[0:4]
+                Month = file[4:6]
+                Day = file[6:8]
+                Hour = file[8:10]
+                Minute = file[10:12]
+                
+                # Format datetime
+                Date = Year + "-" + Month + "-" + Day + ' ' + Hour + ':' + Minute
+                DateTime.append(Date)
 
-        # Store in dataframe
-        df.iloc[filelist.index(file)] = extract_data
+                # Load radar data from file
+                extract_data = np.arange(len(location_list), dtype=float)
+                data_png = Image.open(radar_png_day_path + '/' + file)
+                data_png_numpy = np.array(data_png)
+                df_data = pd.DataFrame(data_png_numpy)
+
+                # Loop over locations of stations
+                for location in location_list:
+                    # Get the data corresponding to the pixel locations
+                    pixel_x = int(location[2])
+                    pixel_y = int(location[1])
+                    value = df_data.iloc[pixel_y, pixel_x]
+                    extract_data[location_list.index(location)] = value
+
+                # Store in dataframe
+                radar_df.loc[len(radar_df)] = extract_data
 
     # Set datetime as index column
     df_datetime = pd.DataFrame(DateTime)
     df_datetime.index = np.arange(1, len(df_datetime)+1)
     data_datetime = df_datetime.astype('datetime64[ns]')
-    df.insert(loc=0, column='Datetime', value=data_datetime)
-    df.set_index('Datetime', inplace=True)
+    radar_df.insert(loc=0, column='Datetime', value=data_datetime)
+    radar_df.set_index('Datetime', inplace=True)
+    radar_df = radar_df.sort_index(axis=1)
 
     # Set data to Thai local time
-    df = df.shift(7, freq='H')
+    radar_df = radar_df.shift(7, freq='H')
 
     # Filter noise and hail
-    df[df < noise_threshold] = 0
-    df[df > hail_threshold] = hail_threshold
+    radar_df[radar_df < noise_threshold] = 0
+    radar_df[radar_df > hail_threshold] = hail_threshold
 
     # Convert dBZ to Z
-    df = 10**(df/10)
-    df = df.replace(1,0)
+    radar_df = 10**(radar_df/10)
+    radar_df = radar_df.replace(1,0)
 
     # Average over hours
-    df = df.resample('H').mean()
+    radar_df = radar_df.resample('H').mean()
 
-    return df
+    return radar_df
